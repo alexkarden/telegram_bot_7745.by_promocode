2ed216f8f32091f4dd86faf815080e15fe1c0ae4
Alex Karden <61961749+alexkarden@users.noreply.github.com>
2025-05-01 23:51:36 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ TOKEN='Token'
 URL7745='https://7745.by/articles/ispolzovanie-promokoda'
 # Через какой интервал ( в минутах) надо проверять страницу с URL на наличие новых промокодов
 CHECKINTERVAL=1
+
 # Список администраторов
 ADMINLIST=[]
 
